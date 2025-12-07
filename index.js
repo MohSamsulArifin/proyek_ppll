9b07efc8169f9661133ffc8766683a4382076f40
sammarifin <mohsamsularifin684@gmail.com>
2025-12-07 13:30:44 +0700

    fix listen port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // PENTING: pakai port dari Railway
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('<h1>WEB SUDAH JALAN ✅</h1>');
